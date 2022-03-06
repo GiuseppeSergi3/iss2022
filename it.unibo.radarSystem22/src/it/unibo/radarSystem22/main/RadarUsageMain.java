f8ca4ae8a2e1ae3845fb28903448789e7859b52b
GiuseppeSergi3 <giuseppe.sergi5@studio.unibo.it>
2022-03-06 12:25:14 +0100

    RadarSystem

--- it.unibo.radarSystem22/src/it/unibo/radarSystem22/main/RadarUsageMain.java
+++ it.unibo.radarSystem22/src/it/unibo/radarSystem22/main/RadarUsageMain.java
@@ -1,11 +1,38 @@
 package it.unibo.radarSystem22.main;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import radarPojo.radarSupport;
 
 public class RadarUsageMain {
     public void doJob() {
+    		try {
+				ServerSocket sSocket=new ServerSocket(12345);
+				Socket cSocket = sSocket.accept();
+			
             System.out.println("start");
             radarSupport.setUpRadarGui();
-            radarSupport.update( "40", "60");
+            int i=0;
+            while (true)
+            {
+
+            	InputStream inputStream = cSocket.getInputStream();
+            	DataInputStream dataInputStream= new DataInputStream(inputStream);
+            	Double message = dataInputStream.readDouble();
+            	System.out.println("RICEVUTO: "+message);
+            	i++;
+            	radarSupport.update( ""+message, "0");
+            }
+            //radarSupport.update( "40", "60");
+    		} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
     }
     public static void main(String[] args) {
             new RadarUsageMain().doJob();
